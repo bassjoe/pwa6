92f1c4ce9ff3f258d6fb5e74079b8fa95f4cee51
bassjoe <18555116+bassjoe@users.noreply.github.com>
2023-12-28 19:29:46 +0100

    pwa

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<svelte:head>
+    <title>PWA #6</title>
+    <meta name="description" content="This is PWA #6." />
+</svelte:head>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,33 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			manifest: {
+				name: 'pwa6-n',
+				short_name: 'pwa6-s',
+				description: 'pwa #6',
+				display: 'standalone',
+				theme_color: '#eff',
+				background_color: '#fff',
+				icons: [
+					{
+						src: 'icon-192.png',
+						sizes: '192x192',
+						type: 'image/png',
+						purpose: 'maskable'
+					},
+					{
+						src: 'icon-512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					}
+				]
+			},
+			registerType: 'autoUpdate',
+		})
+	]
 });
