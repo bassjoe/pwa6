4532722b9f9c0adadc42614ce9baaea464bc84db
bassjoe <18555116+bassjoe@users.noreply.github.com>
2023-12-31 11:30:04 +0100

    trailing slash in nav

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -6,8 +6,8 @@
 <nav>
     <p>Current URL: {$page.url.pathname}</p>
     <a href="{base}/" class:nav-active-route={$page.url.pathname === `${base}/`}>Home</a>
-    <a href="{base}/about" class:nav-active-route={$page.url.pathname === `${base}/about/`}>About</a>
-    <a href="{base}/sign-up" class:nav-active-route={$page.url.pathname === `${base}/sign-up/`}>Sign up</a>
+    <a href="{base}/about/" class:nav-active-route={$page.url.pathname === `${base}/about/`}>About</a>
+    <a href="{base}/sign-up/" class:nav-active-route={$page.url.pathname === `${base}/sign-up/`}>Sign up</a>
 </nav>
 
 <style>
